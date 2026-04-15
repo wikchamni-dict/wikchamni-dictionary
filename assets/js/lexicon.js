aa8b6e947380c4093f05b583696947ef4f1d5462
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2026-04-15 10:09:18 -0700

    select color palette and finish desktop mode display

--- assets/js/lexicon.js
+++ assets/js/lexicon.js
@@ -1,6 +1,12 @@
 
 import * as parser from './parse-toolbox-output.js';
 
+const CARD_TYPE_ENTRY = false;
+const CARD_TYPE_LEXEME = true;
+
+const RE_SYNONYM_SPLITTER = /;\s*/;
+const SYNONYM_JOIN = '; ';
+
 // data
 const CATG_ABBRS = Object.freeze({
     'n-theme' : 'N-Thm',
@@ -22,19 +28,34 @@ const CHAR_PRONUNCIATION = Object.freeze({
     '·' : `[·] makes the previous vowel twice as long.`,
 });
 // helpers
-const capitalize = (text) => (text[0]?.toUpperCase() ?? '') + text.slice(1);
+const capitalize = (text='') => (text[0]?.toUpperCase() ?? '') + text.slice(1);
 // const capitalizeMulti = (text) => text.replaceAll(/\b[a-z]/g, c => c.toUpperCase());
+const capitalizeMulti = (text='') => text.replaceAll(/(?:^|[^\w'])([a-z])/g, c => c.toUpperCase());
+// console.log(capitalizeMulti('hello world hi-ho it\'s off To Work,we;go'));
 
 // DOM anchors
 const eCopyChars = document.querySelector('#search-copy-chars');
 const eSearchResults = document.querySelector('#search-results-wrapper');
 const eNumResults = document.querySelector('#search-stat-num-results');
 const eSearchTime = document.querySelector('#search-stat-time');
+const eEntryDisplay = document.querySelector('#dictionary-entry');
 // object-pooled DOM elements (reuseable)
 const eSearchResultsStatus = Object.assign(document.createElement('p'), {
     classList : `search-results-status`,
     textContent : `No results...`
 });
+
+// audio
+let audioPlayer = {
+	player : document.createElement('audio'),
+	projectPath : '', // leave blank to use relative paths
+	play : (src) => {
+		console.log(`Playing audio "${audioPlayer.projectPath}/${src}"`);
+		audioPlayer.player.pause();
+		audioPlayer.player.src = `${audioPlayer.projectPath}/${src}`;
+		audioPlayer.player.play();
+	}
+};
 
 
 
@@ -51,25 +72,22 @@ const t1_page = performance.now();
 
 //// INDEX DATA ////
 
-let indexL1 = {}; // index['word'] = IndexCard
-let indexL2 = {};
-let orderedL1 = []; // alphabetized array of uniq words (case-insensative)
-let orderedL2 = [];
+let indexL1 = []; // index[entryId] = IndexCard{}; alphabetized by case-insensative (word,catg)
+let indexL2 = [];
 
-// index data for fast searching
-const IndexCard = (word='',catg='',hasAudio=false,hasImages=false) => {
-    return { // cast as string to harden against injection
+const IndexCard = (type,id,word='',catg='',hasAudio=false,hasImages=false) => {
+    return { // typecast to harden against injection
+        isLexeme : !!type, // standard entry or lexeme
+        id : parseInt(id), // entry id in parse.entries[] or parse.lexemes[]
         word : new String(word),
         catg : new String(catg),
-        entries : new Set(),
-        lexemes : new Set(),
-        hasAudio : hasAudio,
-        hasImages : hasImages,
-        domElement : null, // pointer to entry in search results
+        hasAudio : !!hasAudio,
+        hasImages : !!hasImages,
+        domElement : null, // pointer to DOM elem in search results
     }
 };
+
 // index standard entries
-const t0_index = performance.now();
 for (let i = 0; i < parse.entries.length; i++) {
     // scan media
     let hasImages = false;
@@ -88,72 +106,140 @@ for (let i = 0; i < parse.entries.length; i++) {
         }
     }
     // scan words
-    for (let L1 of parse.entries[i].L1 ?? []) {
-        // access is 1-deep, with non-arbitrary value
-        if (!indexL1[L1]) indexL1[L1] = IndexCard(L1, parse.entries[i].catg, hasAudio, hasImages);
-        indexL1[L1].entries.add(i);
+    for (let raw of parse.entries[i].L1 ?? []) {
+        const synonyms = raw.split(RE_SYNONYM_SPLITTER);
+        if (synonyms.length > 1) console.error(`The word "${raw}" in L1 of entry ${i} conatined semicolon-separated words.`);
+        for (let L1 of synonyms) {
+            indexL1.push(
+                IndexCard(CARD_TYPE_ENTRY,i,L1,parse.entries[i].catg,hasAudio,hasImages)
+            );
+        }
     }
-    for (let wordform of parse.entries[i].L2 ?? []) {
-        if (!indexL2[wordform.L2]) indexL2[wordform.L2] = IndexCard(wordform.L2, parse.entries[i].catg, hasAudio, hasImages);
-        indexL2[wordform.L2].entries.add(i);
+    for (let raw of parse.entries[i].L2 ?? []) {
+        const synonyms = raw.L2.split(RE_SYNONYM_SPLITTER);
+        if (synonyms.length > 1) console.error(`The word "${raw}" in L2 of entry ${i} conatined semicolon-separated words.`);
+        for (let L2 of synonyms) {
+            indexL2.push(
+                IndexCard(CARD_TYPE_ENTRY,i,L2,parse.entries[i].catg,hasAudio,hasImages)
+            );
+        }
     }
 }
 // index lexeme entries
 for (let i = 0; i < parse.lexemes.length; i++) {
-    for (let L1 of parse.entries[i].L1 ?? []) {
-        // access is 1-deep, with non-arbitrary value
-        if (!indexL1[L1]) indexL1[L1] = IndexCard(L1, parse.entries[i].catg);
-        indexL1[L1].entries.add(i);
+    // scan media
+    let hasImages = false;
+    let hasAudio = false;
+    if (parse.lexemes[i].images?.length > 0) {
+        hasImages = true;
+        console.log(`${parse.lexemes[i].images.length} image files in lexeme-type entry ${i}`);
     }
-    for (let wordform of parse.lexemes[i].L2 ?? []) {
-        if (!indexL2[wordform.L2]) indexL2[wordform.L2] = IndexCard(wordform.L2, parse.entries[i].catg);
-        indexL2[wordform.L2].lexemes.add(i);
+    for (let L2 of parse.lexemes[i].L2 ?? []) {
+        if (L2.audio?.length > 0) {
+            hasAudio = true;
+            console.log(`${L2.audio.length} audio files from wordform "${L2.L2}" in lexeme-type entry ${i}`);
+            break;
+        }
+    }
+    for (let example of parse.lexemes[i].examples ?? []) {
+        if (example.audio?.length > 0) {
+            hasAudio = true;
+            console.log(`${example.audio.length} audio files from wordform "${example.L2}" in lexeme-type entry ${i}`);
+            break;
+        }
+    }
+    // scan words
+    for (let raw of parse.lexemes[i].L1 ?? []) {
+        const synonyms = raw.split(RE_SYNONYM_SPLITTER);
+        if (synonyms.length > 1) console.error(`The word "${raw}" in L1 of lexeme ${i} conatined semicolon-separated words.`);
+        for (let L1 of synonyms) {
+            indexL1.push(
+                IndexCard(CARD_TYPE_LEXEME,i,L1,parse.lexemes[i].catg,hasAudio,hasImages)
+            );
+        }
+    }
+    for (let raw of parse.lexemes[i].L2 ?? []) {
+        const synonyms = raw.L2.split(RE_SYNONYM_SPLITTER);
+        if (synonyms.length > 1) console.error(`The word "${raw}" in L2 of lexeme ${i} conatined semicolon-separated words.`);
+        for (let L2 of synonyms) {
+            indexL2.push(
+                IndexCard(CARD_TYPE_LEXEME,i,L2,parse.lexemes[i].catg,hasAudio,hasImages)
+            );
+        }
     }
 }
-// sort index cards alphabetically (case insensative)
-orderedL1 = Object.keys(indexL1).sort(
-    (a,b) => {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
-    }
-);
-console.log(`Ordered L1:`, orderedL1);
-orderedL2 = Object.keys(indexL2).sort(
-    (a,b) => {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
-    }
-);
-console.log(`Ordered L2:`, orderedL2);
-const t1_index = performance.now();
-console.log(`Data indexed in ${Math.round(t1_index-t0_index)} ms`);
+const t2_page = performance.now();
+console.log(`Entries indexed in ${Math.round(t2_page-t1_page)} ms.`);
+
+// alphabetize index cards by (word,catg)
+indexL1.sort((a,b) => {
+    const keyA = `${a.word.toLowerCase()}${SYNONYM_JOIN}${a.catg.toLowerCase()}`;
+    const keyB = `${b.word.toLowerCase()}${SYNONYM_JOIN}${b.catg.toLowerCase()}`;
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+});
+console.log(indexL1);
+indexL2.sort((a,b) => {
+    const keyA = `${a.word.toLowerCase()}${SYNONYM_JOIN}${a.catg.toLowerCase()}`;
+    const keyB = `${b.word.toLowerCase()}${SYNONYM_JOIN}${b.catg.toLowerCase()}`;
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+});
+console.log(indexL2);
+const t3_page = performance.now();
+console.log(`Index cards alphabetized in ${Math.round(t3_page-t2_page)} ms.`);
+
 // build dom elems from template
-for (let word in indexL1) {
-    indexL1[word].domElement = document.getElementById('tpl-search-result').content.firstElementChild.cloneNode(true);
-    const card = indexL1[word];
-    const e = indexL1[word].domElement;
+for (let card of indexL1) {
+    const entry = (card.isLexeme) ? parse.lexemes[card.id] : parse.entries[card.id];
+    if (!entry) {
+        console.error(`Index card points to ${(card.isLexeme?'lexeme ':'')}entry that doesn't exist.`, card);
+        continue;
+    }
+    card.domElement = document.getElementById('tpl-search-result').content.firstElementChild.cloneNode(true);
+    const e = card.domElement;
     e.querySelector('.search-result-catg').textContent = CATG_ABBRS[card.catg] ?? capitalize(card.catg);
     e.querySelector('.search-result-word').textContent = card.word;
-    if (card.hasImages) e.querySelector('.icon:nth-of-type(1)').classList.add('icon-dbg');
-    if (card.hasAudio) e.querySelector('.icon:nth-of-type(2)').classList.add('icon-dbg');
+    if (card.hasImages) {
+        e.querySelector('.icon:nth-of-type(1)').classList.add('icon-image');
+        e.querySelector('.icon:nth-of-type(1)').title = 'Has image(s)';
+    }
+    if (card.hasAudio) {
+        e.querySelector('.icon:nth-of-type(2)').classList.add('icon-audio');
+        e.querySelector('.icon:nth-of-type(2)').title = 'Has audio';
+    }
+    e.onclick = () => {
+        console.log(`Rendering ${(card.isLexeme?'lexeme ':'')}entry ${card.id} for "${card.word}" (${card.catg})`);
+        renderEntryFor(card);
+    };
 }
-for (let word in indexL2) {
-    indexL2[word].domElement = document.getElementById('tpl-search-result').content.firstElementChild.cloneNode(true);
-    const card = indexL2[word];
-    const e = indexL2[word].domElement;
+for (let card of indexL2) {
+    const entry = (card.isLexeme) ? parse.lexemes[card.id] : parse.entries[card.id];
+    if (!entry) {
+        console.error(`Index card points to ${(card.isLexeme?'lexeme ':'')}entry that doesn't exist.`, card);
+        continue;
+    }
+    card.domElement = document.getElementById('tpl-search-result').content.firstElementChild.cloneNode(true);
+    const e = card.domElement;
     e.querySelector('.search-result-catg').textContent = CATG_ABBRS[card.catg] ?? capitalize(card.catg);
     e.querySelector('.search-result-word').textContent = card.word;
-    if (card.hasImages) e.querySelector('.icon:nth-of-type(1)').classList.add('icon-dbg');
-    if (card.hasAudio) e.querySelector('.icon:nth-of-type(2)').classList.add('icon-dbg');
+    if (card.hasImages) {
+        e.querySelector('.icon:nth-of-type(1)').classList.add('icon-image');
+        e.querySelector('.icon:nth-of-type(1)').title = 'Has image(s)';
+    }
+    if (card.hasAudio) {
+        e.querySelector('.icon:nth-of-type(2)').classList.add('icon-audio');
+        e.querySelector('.icon:nth-of-type(2)').title = 'Has audio';
+    }
+    e.onclick = () => {
+        console.log(`Rendering ${(card.isLexeme?'lexeme ':'')}entry ${card.id} for "${card.word}" (${card.catg})`);
+        renderEntryFor(card);
+    };
 }
-const t2_index = performance.now();
-console.log(`Search results cards built in ${Math.round(t2_index-t1_index)} ms`);
+const t4_page = performance.now();
+console.log(`Search result DOM elements built in ${Math.round(t4_page-t3_page)} ms`);
 
 
 
@@ -199,14 +285,14 @@ let search = {
     populate : () => {
         eSearchResults.textContent = '';
         if (search.toggleLang) {
-            for (let word of orderedL2) {
-                indexL2[word].domElement.classList.remove('hidden');
-                eSearchResults.appendChild(indexL2[word].domElement);
+            for (let card of indexL2) {
+                card.domElement.classList.remove('hidden');
+                eSearchResults.appendChild(card.domElement);
             }
         } else {
-            for (let word of orderedL1) {
-                indexL1[word].domElement.classList.remove('hidden');
-                eSearchResults.appendChild(indexL1[word].domElement);
+            for (let card of indexL1) {
+                card.domElement.classList.remove('hidden');
+                eSearchResults.appendChild(card.domElement);
             }
         }
         eSearchResultsStatus.classList.add('hidden');
@@ -220,21 +306,21 @@ let search = {
         const RE_PATTERN = new RegExp(pattern);
         let numMatches = 0;
         if (search.toggleLang) {
-            for (let word of orderedL2) {
-                if (RE_PATTERN.test(word)) {
+            for (let card of indexL2) {
+                if (RE_PATTERN.test(card.word.toLowerCase())) {
                     numMatches++;
-                    indexL2[word].domElement.classList.remove('hidden');
+                    card.domElement.classList.remove('hidden');
                 } else {
-                    indexL2[word].domElement.classList.add('hidden');
+                    card.domElement.classList.add('hidden');
                 }
             }
         } else {
-            for (let word of orderedL1) {
-                if (RE_PATTERN.test(word)) {
+            for (let card of indexL1) {
+                if (RE_PATTERN.test(card.word.toLowerCase())) {
                     numMatches++;
-                    indexL1[word].domElement.classList.remove('hidden');
+                    card.domElement.classList.remove('hidden');
                 } else {
-                    indexL1[word].domElement.classList.add('hidden');
+                    card.domElement.classList.add('hidden');
                 }
             }
         }
@@ -243,10 +329,9 @@ let search = {
         } else {
             eSearchResultsStatus.classList.add('hidden');
         }
-        console.log(eSearchResultsStatus);
         const dt = Math.round(performance.now()-t0_filter);
         console.log(`Found ${numMatches} matches in ${dt} ms.`);
-        eNumResults.textContent = `${numMatches}/${(search.toggleLang)?orderedL2.length:orderedL1.length} matches`;
+        eNumResults.textContent = `${numMatches}/${(search.toggleLang)?indexL2.length:indexL1.length} entries`;
         eSearchTime.textContent = `${dt/1000} sec`;
     },
     clear : () => {
@@ -321,14 +406,92 @@ for (let c of COPYABLE_CHARS) {
 
 
 
+//// ENTRY DISPLAY ////
+
+const renderEntryFor = (card) => {
+    const entry = (card.isLexeme) ? parse.lexemes[card.id] : parse.entries[card.id];
+    if (!entry) {
+        console.warn(`Card does not point to a valid ${(card.isLexeme?'lexeme ':'')}entry. Nothing to render.`);
+        return;
+    }
+    // console.log(entry);
+    // header (L1, catg, headword)
+    eEntryDisplay.querySelector('.entry-L1').textContent = entry?.L1.join(SYNONYM_JOIN) || '[no English translation]';
+    eEntryDisplay.querySelector('.entry-catg').textContent = entry.catg || '[no catg]';
+    eEntryDisplay.querySelector('.entry-headword').textContent = entry?.L2[0]?.L2 || '[no Wikchamni headword]';
+    // L2 wordforms
+    if (entry.L2.length > 0) {
+        eEntryDisplay.querySelector('.entry-words').textContent = '';
+        for (let wordform of entry.L2 ?? []) {
+            console.log(`"${wordform.L2}" has form "${wordform.form}"`);
+            const e = document.getElementById('tpl-entry-L2').content.firstElementChild.cloneNode(true);
+            // console.log(wordform.form);
+            e.querySelector('.entry-L2-form').textContent = parser.getWordformName(wordform.form) || capitalizeMulti(wordform.form) || 'Unknown';
+            e.querySelector('.entry-L2-word').textContent = wordform.L2 || '[no Wikchamni translation]';
+            if (wordform.audio?.length > 0) {
+                for (let audio of wordform.audio) {
+                    e.appendChild(
+                        Object.assign(document.createElement('button'), {
+                            classList : 'icon icon-audio',
+                            title : audio || '[blank or missing audio]',
+                            onclick : () => {
+                                console.log(`Play audio "${audio}"`);
+                            }
+                        })
+                    );
+                }
+            }
+            eEntryDisplay.querySelector('.entry-words').appendChild(e);
+        }
+    } else {
+        eEntryDisplay.querySelector('.entry-words').textContent = '[no Wikchamni wordforms]';
+    }
+    // L2 examples
+    if (entry.L2.length > 0) {
+        eEntryDisplay.querySelector('.entry-examples').textContent = '';
+        for (let example of entry.examples ?? []) {
+            const e = document.getElementById('tpl-entry-example').content.firstElementChild.cloneNode(true);
+            e.querySelector('.entry-example-L1').textContent = example.L1 || '[no English translation]';
+            e.querySelector('.entry-example-L2').textContent = example.L2 || '[no Wikchamni translation]';
+            if (example.audio?.length > 0) {
+                for (let audio of example.audio) {
+                    e.querySelector('.flex-row-centered').appendChild(
+                        Object.assign(document.createElement('button'), {
+                            classList : 'icon icon-audio',
+                            title : audio || '[blank or missing audio]',
+                            onclick : () => {
+                                console.log(`Play audio "${audio}"`);
+                            }
+                        })
+                    );
+                }
+            }
+            eEntryDisplay.querySelector('.entry-examples').appendChild(e);
+        }
+    } else {
+        eEntryDisplay.querySelector('.entry-examples').textContent = '[no examples]';
+    }
+    // related words
+    eEntryDisplay.querySelector('.entry-lexemes').textContent = '';
+
+    // TODO: display case names
+    // TOOD: display lexeme / not lexeme indicator
+    // TODO: display images
+    // TODO: if standard entry, display lexeme(s)
+    // TODO: if lexeme entry, L2 words link to entry (if available, else red text)
+};
+
+
+
 //// INIT ////
 
-search.domElement.value = '';
+search.domElement.value = ''; // need to reset input text, since we can't save begins-contains-ends state
 search.populate();
+// search.filter();
 
-const t2_page = performance.now();
-eNumResults.textContent = `${(search.toggleLang)?orderedL2.length:orderedL1.length} matches`;
-eSearchTime.textContent = `Loaded in ${Math.round(t2_page-t1_page)/1000} sec`;
+const t5_page = performance.now();
+eNumResults.textContent = `${(search.toggleLang)?indexL2.length:indexL1.length} entries`;
+eSearchTime.textContent = `Loaded in ${Math.round(t5_page-t4_page)/1000} sec`;
 
 
 
